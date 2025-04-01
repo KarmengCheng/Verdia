21cb8f094b34a4f9711f441c2f97993554d9fffa
MakWeiJen <weijen.mak@unienrol.com>
2025-04-01 16:00:40 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "@/app/globals.css";
+import Image from "next/image";
+import bgImage from "@/assets/bg-image.png";
 
 export default function Home() {
   return (
-    <div className="w-full justify-center items-center align-center">
-      <div className="text-3xl font-bold">LeafLens AI</div>
-    </div>
+    <section className="relative min-h-screen">
+      <Image
+        src={bgImage}
+        alt="Background"
+        fill
+        className="object-cover z-0"
+        quality={100}
+      />
+      <div className="relative z-10 w-full justify-center items-center align-center flex"></div>
+    </section>
   );
 }
